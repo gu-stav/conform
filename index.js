b5366473a6937ef60ea1a14bed56b66e40091095
Gustav Pursche <gustavpursche@gmail.com>
2015-07-11 20:56:43 +0200

    Add missing exports for Fieldset and Legend

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Button = require('./widget/button');
+const Fieldset = require('./widget/fieldset');
 const Form = require('./widget/form');
 const Input = require('./widget/input');
 const Label = require('./widget/label');
+const Legend = require('./widget/legend');
 const RadioChoice = require('./widget/radio-choice');
 const Select = require('./widget/select');
 const Option = require('./widget/option');
@@ -9,9 +11,11 @@ const Textarea = require('./widget/textarea');
 
 module.exports = {
   Button: Button,
+  Fieldset: Fieldset,
   Input: Input,
   Form: Form,
   Label: Label,
+  Legend: Legend,
   RadioChoice: RadioChoice,
   Select: Select,
   Textarea: Textarea,
