676c9f7c0e2d19c4a294d450eccd4fac56a62aca
Gustav Pursche <gustavpursche@gmail.com>
2015-07-11 21:35:05 +0200

    Extract validate-fields to own file

--- widget/form.js
+++ widget/form.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 const Factory = require('./factory');
-const Promise = require('bluebird');
+const validateFields = require('../lib/validate-fields');
 
 var Form = function() {
   this.init.apply(this, arguments);
@@ -25,19 +25,7 @@ Form.prototype = {
   },
 
   validate: function(data) {
-    if(!data) {
-      data = {};
-    }
-
-    var fieldPromises = _.map(this.fields, function(field) {
-      return field.validate(data[field.attributes.name] || undfined);
-    });
-
-    return Promise
-            .all(fieldPromises)
-            .then(function(errors) {
-              return _.flatten(errors);
-            });
+    return validateFields(this.fields, data);
   },
 };
 
