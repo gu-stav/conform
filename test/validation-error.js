ae6b03d76dd222712a8f8b6c0a986a49766a0a9e
Gustav Pursche <gustavpursche@gmail.com>
2015-07-23 23:59:23 +0200

    Error instance remembers the field (for creating error summaries ...)

--- test/validation-error.js
+++ test/validation-error.js
@@ -1,4 +1,6 @@
 const assert = require('assert');
+const Input = require('../widget/input');
+const Promise = require('bluebird');
 const ValidationError = require('../lib/error').ValidationError;
 
 describe('Validation Error', function() {
@@ -14,5 +16,29 @@ describe('Validation Error', function() {
 
     assert.ok(error instanceof ValidationError);
     assert.ok(error instanceof Error);
+  });
+
+  it('should keep the field', function() {
+    var input = new Input({
+      name: 'test',
+      id: 'someid',
+    });
+
+    input.validation({
+      notEmpty: function(val) {
+        return new Promise(function(resolve, reject) {
+          if(!val) {
+            throw new Error('Field was empty');
+          }
+        });
+      },
+    });
+
+    input.validate(null).then(function(errors) {
+      assert.equal(errors.length, 1);
+      assert.equal(errors[0].message, 'Field was empty');
+      assert.equal(errors[0].field.attr('name'), 'test');
+      assert.equal(errors[0].field.attr('id'), 'someid');
+    });
   });
 });
